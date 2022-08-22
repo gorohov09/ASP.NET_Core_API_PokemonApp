a21476a31114277b8e7c72868f52bb9d984a6927
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-22 18:13:30 +0500

    Создание сущности - покемон

---
+++ ASP.NET_Core_API_PokemonApp/Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NET_Core_API_PokemonApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
