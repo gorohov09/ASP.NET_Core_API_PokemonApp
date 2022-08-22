6f3251ca77962cdc1248c713195b4a6563c06829
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-22 18:17:37 +0500

    Создание сущности - владелец

---
+++ ASP.NET_Core_API_PokemonApp/Models/Owner.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NET_Core_API_PokemonApp.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gym { get; set; }
+    }
+}
