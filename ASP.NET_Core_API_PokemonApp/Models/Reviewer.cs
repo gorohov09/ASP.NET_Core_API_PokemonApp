2896b003fd167d68e1205fd1eee06b0bc45e5bdf
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-22 18:25:57 +0500

    Создание сущности - эксперт

---
+++ ASP.NET_Core_API_PokemonApp/Models/Reviewer.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NET_Core_API_PokemonApp.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
